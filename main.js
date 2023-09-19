5c2f4f35497a07e2d5c106fc117b5f9b1ea0f7b1
BarbatosLopux <sneideresteban@gmail.com>
2023-09-19 13:11:16 -0500

    feat: :tada: First Commit
    Trabajo entregar API's

---
+++ main.js
@@ -0,0 +1,39 @@
+let myfrom = document.querySelector("form");
+let myTbla = document.querySelector("#mydata");
+addEventListener("DOMContentLoaded", async()=>{
+    let res = await (await fetch("https://6509d043f6553137159c1059.mockapi.io/tabla")).json();
+    for (let i = 0; i < res.length; i++) {
+        myTbla.insertAdjacentHTML ("beforeend", ` 
+        <tr> 
+            <td>${res[i].id}</td>
+            <td>${res[i].valor}</td>
+            <td>${res[i].caja}</td>        
+        </tr>
+        
+        
+        
+        
+        `)
+
+})
+
+/* myfrom.addEventListener("submit", (e) => {
+    console.log(e);
+    e.preventDefault();
+})
+ */
+
+myfrom.addEventListener("submit", (e) => {
+    const data = Object.fromEntries(new FormData(e.target));
+    const{caja, ...datanueva} = data
+    console.log(datanueva);
+    e.preventDefault();
+
+})
+
+const obtener=() => {
+    let api = fetch ('https://6509d043f6553137159c1059.mockapi.io/tabla')
+    console.log(api);
+}
+obtener() 
+
