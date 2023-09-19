64ac059d74d75bc06f47d98db52973e4e5dbbd10
BarbatosLopux <sneideresteban@gmail.com>
2023-09-19 13:31:30 -0500

    feat: :sparkles: COMMIT ACTUALIZACION JAVASCRIPT
    Toca correguir codigo

--- main.js
+++ main.js
@@ -9,13 +9,21 @@ addEventListener("DOMContentLoaded", async()=>{
             <td>${res[i].valor}</td>
             <td>${res[i].caja}</td>        
         </tr>
-        
-        
-        
-        
-        `)
+        `);
+    }
+})
+myfrom.addEventListener("submit", async(e)=>{
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target));
+    let config = {
+        method : "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify(data)
+    };
+    let res = await (await fetch("https://6509d043f6553137159c1059.mockapi.io/tabla"))
 
 })
+
 
 /* myfrom.addEventListener("submit", (e) => {
     console.log(e);
