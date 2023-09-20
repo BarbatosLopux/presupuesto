c4dc43abdda249e340d0507e6bbe7c88b7ce7019
BarbatosLopux <sneideresteban@gmail.com>
2023-09-20 13:28:07 -0500

    feat: :sparkles: Modificacion HTML y JS
    Se metio la opcion de eliminar y ver el contenido dentro de la tabla

--- main.js
+++ main.js
@@ -1,47 +1,72 @@
-let myfrom = document.querySelector("form");
-let myTbla = document.querySelector("#mydata");
-addEventListener("DOMContentLoaded", async()=>{
-    let res = await (await fetch("https://6509d043f6553137159c1059.mockapi.io/tabla")).json();
+const myform = document.querySelector("form");
+const myTable = document.querySelector("#myData");
+
+async function mostrarDatos() {
+    let res = await (await fetch("https://6509d0e4f6553137159c123e.mockapi.io/tabla")).json();
+    console.log(res);
+
+    myTable.innerHTML = ""; 
+
     for (let i = 0; i < res.length; i++) {
-        myTbla.insertAdjacentHTML ("beforeend", ` 
-        <tr> 
+        const row = document.createElement("tr");
+        row.innerHTML = `
             <td>${res[i].id}</td>
             <td>${res[i].valor}</td>
-            <td>${res[i].caja}</td>        
-        </tr>
-        `);
+            <td>${res[i].caja}</td>
+            <td>
+                <button class="btn-eliminar" data-id="${res[i].id}">Eliminar</button>
+            </td>
+        `;
+
+        myTable.appendChild(row);
     }
-})
-myfrom.addEventListener("submit", async(e)=>{
+}
+
+addEventListener("DOMContentLoaded", mostrarDatos);
+
+myTable.addEventListener("click", function (event) {
+    if (event.target.classList.contains("btn-eliminar")) {
+        const idAEliminar = event.target.getAttribute("data-id");
+
+        fetch(`https://6509d0e4f6553137159c123e.mockapi.io/tabla/${idAEliminar}`, {
+            method: "DELETE"
+        })
+        .then((response) => {
+            if (response.ok) {
+                mostrarDatos();
+            } else {
+                console.error("Error al eliminar el registro en el servidor.");
+            }
+        })
+        .catch((error) => {
+            console.error("Error al comunicarse con el servidor:", error);
+        });
+    }
+});
+
+myform.addEventListener("submit", async (e) => {
     e.preventDefault();
     const data = Object.fromEntries(new FormData(e.target));
+    const { valor } = data;
+    data.valor = (typeof valor === "string") ? Number(valor) : null;
+
     let config = {
-        method : "POST",
-        headers: {"content-type": "application/json"},
+        method: "POST",
+        headers: { "content-type": "application/json" },
         body: JSON.stringify(data)
     };
-    let res = await (await fetch("https://6509d043f6553137159c1059.mockapi.io/tabla"))
 
-})
+    let res = await (await fetch("https://6509d0e4f6553137159c123e.mockapi.io/tabla", config)).json();
+    console.log(res);
+    window.location.reload();
+});
 
 
-/* myfrom.addEventListener("submit", (e) => {
-    console.log(e);
-    e.preventDefault();
-})
- */
 
-myfrom.addEventListener("submit", (e) => {
-    const data = Object.fromEntries(new FormData(e.target));
-    const{caja, ...datanueva} = data
-    console.log(datanueva);
-    e.preventDefault();
+/* myform.addEventListener("submit", async (e) => {
+    ) */
 
-})
-
-const obtener=() => {
-    let api = fetch ('https://6509d043f6553137159c1059.mockapi.io/tabla')
-    console.log(api);
-}
-obtener() 
-
+/* futuras acciones: 
+   - Buscar informacion dentro de la tabla 
+   - Modificar información de la tabla
+*/
